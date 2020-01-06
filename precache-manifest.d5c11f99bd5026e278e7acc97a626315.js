1ddf12d999323e998fc440df57b13c6e0a68ace1
icywaffle <synkre@gmail.com>
2020-01-06 12:30:31 -0800

    Updates

---
+++ precache-manifest.d5c11f99bd5026e278e7acc97a626315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82584be3351256ccd26690e3c39810da",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8c16b00fad6f1907c50c",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a9721f1def017fd1f76",
+    "url": "/portfolio/static/js/2.168def68.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.168def68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c16b00fad6f1907c50c",
+    "url": "/portfolio/static/js/main.82ddd8b9.chunk.js"
+  },
+  {
+    "revision": "49553da1696c7dd6ab13",
+    "url": "/portfolio/static/js/runtime-main.60abe820.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
